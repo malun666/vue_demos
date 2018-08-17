154f792c01dd749ed61c5b8b865bff4d190cf50a
malun <malun666@126.com>
2018-08-17 17:39:22 +0800

    :sparkles: added the vue file demo

---
+++ a.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="tt">
+  {{age}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data () {
+    return {
+      age: 19
+    };
+  }
+};
+</script>
+
+<style scoped>
+.tt {
+  color: red;
+}
+</style>
